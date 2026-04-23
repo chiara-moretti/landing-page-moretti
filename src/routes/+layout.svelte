63d7d2c98b8b40b3d03bdd351c3793f5a3e7ef00
chiara-moretti <chiara1.moretti@mail.polimi.it>
2026-04-23 10:37:49 +0200

    add custom fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,16 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<!-- Satoshi Font -->
+	<link rel="preconnect" href="https://api.fontshare.com" />
+	<link rel="preconnect" href="https://api.fontshare.com" crossorigin />
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet" />
+	
+	<!-- Inter Font -->
+	<link rel="preconnect" href="https://fonts.bunny.net">
+	<link rel="preconnect" href="https://fonts.bunny.net" crossorigin>
+	<link href="https://fonts.bunny.net/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
 </svelte:head>
 
 {@render children()}
